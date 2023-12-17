871f99c10ac3cf4944b501ffe9d509d80de70e26
mdshahjhan <mdshahjha478877@gmail.com>
2023-12-17 11:46:39 +0600

    shahjhn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require('dotenv').config
 const cors = require("cors")
-const { MongoClient, ServerApiVersion, LEGAL_TCP_SOCKET_OPTIONS,ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, LEGAL_TCP_SOCKET_OPTIONS, ObjectId } = require('mongodb');
 const { register } = require("module");
 const port = process.env.Port || 5000;
 
@@ -27,33 +27,35 @@ async function run() {
 
     const NewUserCollection = client.db('AddUser').collection('Register');
     const NewloginCollection = client.db('Addlogin').collection('Login');
+    const NewsellerCollection = client.db('AddSeller').collection('create-order');
+    const NewcreateCollection = client.db('Addcreate').collection('create-catalog');
     // all user   login
     app.post('/Register', async (req, res) => {
-        const newUser = req.body;
-        console.log(newUser);
-        const result = await NewUserCollection.insertOne(newUser);
-        res.send(result);
-       
-      });
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await NewUserCollection.insertOne(newUser);
+      res.send(result);
+
+    });
 
 
-      // all user login create api 
+    // all user login create api 
 
-      app.post('/Loing', async (req, res) => {
-        const newLogin = req.body;
-        console.log(newLogin);
-        const result = await NewloginCollection.insertOne(newLogin);
-        res.send(result);
-       
-      });
+    app.post('/Loing', async (req, res) => {
+      const newLogin = req.body;
+      console.log(newLogin);
+      const result = await NewloginCollection.insertOne(newLogin);
+      res.send(result);
+
+    });
 
 
 
-     // all new user get api 
-      app.get('/Register', async (req, res) => {
-        const cursor = NewUserCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    // all new user get api 
+    app.get('/Register', async (req, res) => {
+      const cursor = NewUserCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
     // all login user get api 
@@ -62,10 +64,10 @@ async function run() {
       const cursor = NewloginCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-  })
+    })
 
 
-  
+
 
 
     // seller User
@@ -75,7 +77,7 @@ async function run() {
       const register = await NewUserCollection.findOne(query);
       res.send(register);
     });
-   
+
 
     // all byer 
 
@@ -89,21 +91,50 @@ async function run() {
 
     // seller speiced id 
 
-    
-   
+    app.get('/Register/sellers/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await NewUserCollection.findOne(query);
+      res.send(result);
+    })
+
+    // all Product data 
+    app.post('/create-order/:seller_id', async (req, res) => {
+      const newOrder = req.body;
+      console.log(newOrder);
+      const result = await NewsellerCollection.insertOne(newOrder);
+      res.send(result);
+    })
+
+    // all product get api 
+
+    app.get('/create-order', async (req, res) => {
+      const cursor = NewsellerCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+
+    })
+
+    //
+
+    app.post('/seller/create-catalog', async (req, res) => {
+      const newOrders = req.body;
+      console.log(newOrders);
+      const result = await NewcreateCollection.insertOne(newOrders);
+      res.send(result);
+    })
 
 
 
 
-   
 
 
 
 
- 
 
 
-   
+
+
   }
   finally {
 
